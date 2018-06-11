25bddcaf4392a9017265f509648bea987bdd2f30
Pete Beardmore <pete.beardmore@msn.com>
2018-06-11 13:53:58 +0100

    update price list and fix tests

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,10 +1,16 @@
 import unittest
-from lib.solutions.checkout import checkout
+from lib.solutions.checkout import checkout, build_state
 
 
 class TestCheckout(unittest.TestCase):
+
+    p = None
+
+    def setUp(self):
+        (self.p) = build_state()
+
     def test_checkout(self):
-        self.assertEqual(checkout("AB"), 80)
+        self.assertEqual(checkout("AB"), self.p['A'] + self.p['B'])
 
     def test_checkout2(self):
         self.assertEqual(checkout("AAAAA"), 200)
@@ -13,10 +19,10 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(checkout(" A   B "), -1)
 
     def test_checkout_multi(self):
-        self.assertEqual(checkout("ABA"), 130)
+        self.assertEqual(checkout("ABA"), 2*self.p['A'] + self.p['B'])
 
     def test_checkout_offer(self):
-        self.assertEqual(checkout("AABBA"), 175)
+        self.assertEqual(checkout("AABBA"), 130 + 45)
 
     def test_checkout_illegal(self):
         self.assertEqual(checkout("pandas"), -1)
@@ -25,43 +31,49 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(checkout("Ax"), -1)
 
     def test_checkout_new_product(self):
-        self.assertEqual(checkout("EE"), 80)
+        self.assertEqual(checkout("EE"), 2*self.p['E'])
 
     def test_checkout_bogof(self):
-        self.assertEqual(checkout("BEE"), 80)
+        self.assertEqual(checkout("BEE"), 2*self.p['E'])
 
     def test_checkout_bogof2(self):
-        self.assertEqual(checkout("EEB"), 80)
+        self.assertEqual(checkout("EEB"), 2*self.p['E'])
 
     def test_checkout_bogof_first(self):
-        self.assertEqual(checkout("EEEEBB"), 160)
+        self.assertEqual(checkout("EEEEBB"), 4*self.p['E'])
 
     def test_checkout_bogof_first2(self):
-        self.assertEqual(checkout("BEBEEE"), 160)
+        self.assertEqual(checkout("BEBEEE"), 4*self.p['E'])
 
     def test_checkout_bogof_first3(self):
-        self.assertEqual(checkout("ABCDEABCDE"), 280)
+        self.assertEqual(checkout("ABCDEABCDE"),
+            2*self.p['A'] + self.p['B'] + 2*self.p['C'] + 2*self.p['D'] + 2*self.p['E'])
 
     def test_checkout_2f(self):
-        self.assertEqual(checkout("FF"), 20)
+        self.assertEqual(checkout("FF"), 2*self.p['F'])
 
     def test_checkout_offer_2f(self):
-        self.assertEqual(checkout("FFF"), 20)
+        self.assertEqual(checkout("FFF"), 2*self.p['F'])
 
-    def test_checkout_offers_free_p4(self):
+    def test_checkout_offers_free_p5(self):
+        self.assertEqual(checkout(
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+            sum(x for x in p.values()))
+
+    def test_checkout_offers_free_p5(self):
         self.assertEqual(checkout(
             "EERRRNNNBMQ"),
              80 + 120 + 150)
 
-    def test_checkout_offers_value_p4(self):
+    def test_checkout_offers_value_p5(self):
         self.assertEqual(checkout(
             "AAAAAAAABBFFFHHHHHHHHHHHHHHHKKPPPPPQQQUUUUVVVVV"),
-            200 + 130 + 45 + 20 + 80 + 45 + 150 + 200 + 80 + 120 + 130 + 90)
+            200 + 130 + 45 + 20 + 80 + 45 + 120 + 200 + 80 + 120 + 130 + 90)
 
-    def test_checkout_offers_all_p4(self):
+    def test_checkout_offers_all_p5(self):
         self.assertEqual(checkout(
             "AAAAAAAABBFFFHHHHHHHHHHHHHHHKKPPPPPQQQUUUUVVVVV" +
             "EERRRNNNBMQ"),
-            (200 + 130 + 45 + 20 + 80 + 45 + 150 + 200 + 80 + 120 + 130 + 90) +
+            (200 + 130 + 45 + 20 + 80 + 45 + 120 + 200 + 80 + 120 + 130 + 90) +
             (80 + 120 + 150))
 
