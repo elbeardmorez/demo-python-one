a9c9fc83d21aeb6a71aae33fd56e6de78d3b9dce
Pete Beardmore <pete.beardmore@msn.com>
2018-06-11 09:47:31 +0100

    add sum challenge and tests

--- challenges/sum.py
+++ challenges/sum.py
@@ -1,3 +1,19 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+def validate(val):
+    # type safety
+    if isinstance(val, str):
+        if val.isdigit():
+            val = int(val)
+        else:
+            raise TypeError("input %r must be an integer" % val)
+    # range
+    if val < 0 or val > 100:
+        raise AssertionError("input %r must be within range 0-100" % val)
+    return val
+
 def sum(x, y):
-    raise NotImplementedError()
+    x = validate(x)
+    y = validate(y)    
+    return x + y
+
--- tests/test_sum.py
+++ tests/test_sum.py
@@ -7,6 +7,19 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum(1, 2), 3)
 
+    def test_validate_type(self):
+        with self.assertRaises(TypeError):
+            sum("pandas", 3)
+        with self.assertRaises(TypeError):
+            sum(10, "hippos")
+
+    def test_validate_range(self):
+        with self.assertRaises(AssertionError):
+            sum(1200, 3)
+
+    def test_validate_string_parse(self):
+        self.assertEqual(sum("1", "3"), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
