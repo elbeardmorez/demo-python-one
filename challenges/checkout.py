147694ba35a639251baa4c914b8dc313ce72a790
Pete Beardmore <pete.beardmore@msn.com>
2018-06-11 10:17:47 +0100

    add checkout challenge and tests, part 2

--- challenges/checkout.py
+++ challenges/checkout.py
@@ -2,36 +2,53 @@ from itertools import groupby
 import re
 
 def isdirty(s):
-   return True if re.search("[^A-D]", s) else False
+   return True if re.search("[^A-E]", s) else False
 
-def clean(s):
-   return re.sub("[^A-D]", "")
+#def clean(s):
+#   return re.sub("[^A-E]", "")
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
 
+#  print("considering skus %r" % skus)
   if isdirty(skus):
       return -1
 
-  offers = { 'A': [(3, 130)], 'B': [(2, 45)]}
-  costs = { 'A': 50, 'B': 30, 'C': 20, 'D': 15 }
+  offers = { 'A': [(5, 200, ''), (3, 130, '') ], 'B': [(2, 45, '')], 'E': [(2, 80, 'B')]}
+  costs = { 'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40 }
   a_skus = sorted(list(skus))
 
-  val = 0
+  # group and count
+  counts = {}
   for k, g in groupby(a_skus):  # identity lambda as def
-      count = len(list(g))
-      #print("considering %r, count %d" % (k, count))
-      # consider offers first
+      counts[k] = len(list(g))
+
+  val = 0
+
+  # consider offers first
+  for k, count in counts.items():
+#      print("considering %r, count %d" % (k, count))
       if k in offers:
-          # hmm, possible multiple offers.. order.. best val..
-          for quantity, cost in offers[k]:
-              #print("considering offer on %r, %d for %d" % (k, quantity, cost))
+          # hmm, possible multiple offers.. order matters
+          # TODO don't rely on order matters! sort by best deal value
+          for quantity, cost, bogof in offers[k]:
+#              print("considering offer on %r, %d for %d" % (k, quantity, cost))
               while count >= quantity:
-                  #print("offer requirements met, %d for %d on %r" % (quantity, cost, k))
+#                  print("offer requirements met, %d for %d on %r" % (quantity, cost, k))
                   val += cost
                   count -= quantity
-      # single item cost
+                  counts[k] -= quantity  # outer context
+                  freebies = list(bogof)
+                  if len(freebies) > 0:
+                      # reduce count(s) for given items
+                      for p in freebies:
+                          counts[p] -= 1
+
+  # single item costs only remain
+  for k, count in counts.items():
       val += (count * costs[k])
+
+#  print("returning %r for %r" % (val, skus))
   return val
--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -2,9 +2,12 @@ import unittest
 from lib.solutions.checkout import checkout
 
 
-class TestSum(unittest.TestCase):
+class TestCheckout(unittest.TestCase):
     def test_checkout(self):
         self.assertEqual(checkout("AB"), 80)
+
+    def test_checkout2(self):
+        self.assertEqual(checkout("AAAAA"), 200)
 
     def test_checkout_dirty_input(self):
         self.assertEqual(checkout(" A   B "), -1)
@@ -19,5 +22,14 @@ class TestSum(unittest.TestCase):
         self.assertEqual(checkout("pandas"), -1)
 
     def test_checkout_illegal2(self):
-        self.assertEqual(checkout("AE"), -1)
+        self.assertEqual(checkout("AX"), -1)
+
+    def test_checkout_new_product(self):
+        self.assertEqual(checkout("EE"), 80)
+
+    def test_checkout_bogof(self):
+        self.assertEqual(checkout("BEE"), 80)
+
+    def test_checkout_bogof2(self):
+        self.assertEqual(checkout("EEB"), 80)
 
